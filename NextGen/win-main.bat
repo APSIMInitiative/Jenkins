a19984863d1224ef2376121b81b49fe955376afb
Drew Holzworth <drew.holzworth@csiro.au>
2021-05-18 09:03:14 +1000

    Fixed typo in windows installer script
    Mount path should be relative to script's location, rather than
    current working directory.

--- NextGen/win-main.bat
+++ NextGen/win-main.bat
@@ -5,4 +5,4 @@ rem installer.
 
 set container=apsiminitiative/apsimng-build-win
 docker pull -q %container%
-docker run --rm --entrypoint=deploy-win.bat -v "%CD%\container-scripts":C:/container-scripts -w /container-scripts -e PULL_ID -e APSIM_SITE_CREDS -e APSIM_CERT -e APSIM_CERT_PWD -e APSIM_SITE_CREDS %container%
+docker run --rm --entrypoint=deploy-win.bat -v "%~dp0container-scripts":C:/container-scripts -w /container-scripts -e PULL_ID -e APSIM_SITE_CREDS -e APSIM_CERT -e APSIM_CERT_PWD -e APSIM_SITE_CREDS %container%
