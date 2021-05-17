1951cd97bd65a505b770b5dd00f59c4c8c421f2b
Drew Holzworth <drew.holzworth@csiro.au>
2021-05-17 14:10:00 +1000

    Fixed a typo in the name of the windows docker image.

--- NextGen/win-main.bat
+++ NextGen/win-main.bat
@@ -3,6 +3,6 @@ rem This script run inside a docker container by the Jenkins
 rem infrastructure. It will build and upload the windows .NET Core
 rem installer.
 
-set container=apsiminitiative/apsimng-windows
+set container=apsiminitiative/apsimng-build-win
 docker pull -q %container%
-docker run --rm --entrypoint=deploy-win.bat -v "%CD%\container-scripts":C:/container-scripts -w /container-scripts -e PULL_ID -e APSIM_SITE_CREDS -e APSIM_CERT -e APSIM_CERT_PWD -e APSIM_SITE_CREDS $container "${@:2}"
+docker run --rm --entrypoint=deploy-win.bat -v "%CD%\container-scripts":C:/container-scripts -w /container-scripts -e PULL_ID -e APSIM_SITE_CREDS -e APSIM_CERT -e APSIM_CERT_PWD -e APSIM_SITE_CREDS %container%
