72d623ae959c441c9b25543abdd082442d882dbb
Andrew Paroz <par456@csiro.au>
2025-01-15 11:32:58 +1000

    Update scripts to .net 8.0

--- NextGen/container-scripts/deploy-win.bat
+++ NextGen/container-scripts/deploy-win.bat
@@ -62,16 +62,16 @@ copy /y "%apsimx%\Models\Properties\AssemblyVersion.cs" "%apsimx%\ApsimNG\Proper
 rem Build the solution.
 
 rem Without the -m:1 below, the order of builds can be incorrect sometimes resulting in this order:
-rem     Models -> C:\container-scripts\ApsimX\bin\Release\net6.0\win-x64\Models.dll
-rem     Models -> C:\container-scripts\ApsimX\bin\Release\net6.0\Models.dll
-rem     Models -> C:\container-scripts\ApsimX\bin\Release\net6.0\win-x64\publish\Models.dll
-rem This can lead to an incorrect  publish\models.deps.json. taken from net6.0 directory rather than from netcoreapp3.1\win-x64 directory
+rem     Models -> C:\container-scripts\ApsimX\bin\Release\net8.0\win-x64\Models.dll
+rem     Models -> C:\container-scripts\ApsimX\bin\Release\net8.0\Models.dll
+rem     Models -> C:\container-scripts\ApsimX\bin\Release\net8.0\win-x64\publish\Models.dll
+rem This can lead to an incorrect  publish\models.deps.json. taken from net8.0 directory rather than from netcoreapp3.1\win-x64 directory
 rem bug: https://github.com/APSIMInitiative/ApsimX/issues/7829
-dotnet publish -c Release -f net6.0 -r win-x64 -m:1 --no-self-contained "%apsimx%\ApsimX.sln"
+dotnet publish -c Release -f net8.0 -r win-x64 -m:1 --no-self-contained "%apsimx%\ApsimX.sln"
 if errorlevel 1 exit /b 1
 
 rem Generate the installer.
-set "setup=%apsimx%\Setup\net6.0\windows"
+set "setup=%apsimx%\Setup\net8.0\windows"
 iscc /Q "%setup%\apsimx.iss"
 if errorlevel 1 exit /b 1
 set "INSTALLER=apsim-%REVISION%.exe"
